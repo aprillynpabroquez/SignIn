bb0526fb704cf8468a1082f1444d149ac33e6588
aprillynpabroquez <aprillynpabroquez@sjp2cd.edu.ph>
2025-02-10 14:27:36 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply true
+    alias(libs.plugins.kotlin.android) apply true
+    alias(libs.plugins.kotlin.compose) apply true
+}
